1669ffc1b62016cf4d054029e465022abb152dc8
MarcusHjorth <U773559@rts-365.dk>
2021-08-17 14:33:31 +0200

    Add Solution to assignment 01B

--- eks_01b_DOMogEvents/js/index.js
+++ eks_01b_DOMogEvents/js/index.js
@@ -3,4 +3,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
 
+	let number = 5;
+	let MaximumNummer = 10;
+	let MinimumNummer = 0;
+	let TextCountElement = document.querySelector('#text-count');
+	let ButtonIncrease = document.querySelector('#btn-count-increase')
+	let ButtonDecrease = document.querySelector('#btn-count-decrease')
+	
+	updateNumber ();
+	function updateNumber (){
+		TextCountElement.textContent = number;
+	}
+	
+	
+	ButtonIncrease.addEventListener("click", () => {
+		if(number < MaximumNummer){
+			number = number + 1;
+			updateNumber();
+		}
+	});
+	
+	ButtonDecrease.addEventListener("click", () => {
+		if(number > MinimumNummer){
+		number = number - 1;
+		updateNumber();
+		}
+	});
+
 }); // Afslutter: DOMContentLoaded
