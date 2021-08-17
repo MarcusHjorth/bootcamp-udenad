41ce6917f4dd13e820d607d9d7f3444616ae4400
MarcusHjorth <U773559@rts-365.dk>
2021-08-17 14:11:16 +0200

    Add solution eks_01a

--- eks_01a_DOMogEvents/js/index.js
+++ eks_01a_DOMogEvents/js/index.js
@@ -4,4 +4,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
 
+	let number = 5;
+	let TextCountElement = document.querySelector('#text-count');
+	let ButtonIncrease = document.querySelector('#btn-count-increase')
+	let ButtonDecrease = document.querySelector('#btn-count-decrease')
+	
+	updateNumber ();
+	function updateNumber (){
+		TextCountElement.textContent = number;
+	}
+	
+	
+	ButtonIncrease.addEventListener("click", () => {
+		if(number < 10){
+			number = number + 1;
+			updateNumber();
+		}
+	});
+	
+	ButtonDecrease.addEventListener("click", () => {
+		if(number > 0){
+		number = number - 1;
+		updateNumber();
+		}
+	});
+
+	
+
 }); // Afslutter: DOMContentLoaded
