5a28d2c9e7011ad385ed09a94fc9f3e6f0fcb611
MarcusHjorth <U773559@rts-365.dk>
2021-08-19 15:06:37 +0200

    Add assignment 03a

--- eks_03a_arrays/js/index.js
+++ eks_03a_arrays/js/index.js
@@ -3,4 +3,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
 
+let names = ["Brad", "Chad", "Thad"];
+let ulElement = document.querySelector("#liste");
+
+
+names.forEach((navn) => {
+	ulElement.innerHTML += `<li> ${navn} </li>`
+});
+
+
+//names.forEach((names) => {
+//    let li = document.createElement('li');
+//   ulElement.appendChild(li);
+//    li.innerHTML += names;
+//});
+
+
 }); // Afslutter: DOMContentLoaded
