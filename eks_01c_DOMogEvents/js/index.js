82c6b670117422761ac5f6105cf168783079a093
MarcusHjorth <U773559@rts-365.dk>
2021-08-19 10:04:28 +0200

    Add Assignment 01c

--- eks_01c_DOMogEvents/js/index.js
+++ eks_01c_DOMogEvents/js/index.js
@@ -3,4 +3,60 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
 
+	let number = 5;
+	let MaximumNummer = 10;
+	let MinimumNummer = 0;
+	let ButtonIncrease = document.querySelector('#btn-count-increase')
+	let ButtonDecrease = document.querySelector('#btn-count-decrease')
+	let TextCountElement = document.querySelector('#text-count');
+	
+	
+	function updateNumber (){
+		TextCountElement.textContent = number;
+	}
+	updateNumber();
+
+
+	function checkMinMaxValue(direction) {
+		if(direction === "inc" && number < MaximumNummer){
+			number = number + 1;
+			updateNumber();
+		} 
+		if (direction === "dec" && number > MinimumNummer) {
+			number = number - 1;
+			updateNumber();
+		}
+	}
+	
+
+	ButtonIncrease.addEventListener("click", () => checkMinMaxValue("inc"));
+	ButtonDecrease.addEventListener("click", () => checkMinMaxValue("dec"));
+
+
+
+//	function calcNumber(event){
+//		if(event.target.attributes.id.nodeValue === "btn-count-increase"){
+//			if(number < MaximumNummer){
+//				updateNumber();
+//			}
+//		} else if (event.target.attributes.id.nodeValue === "btn-count-decrease"){
+//			if(number > MinimumNummer){
+//				number = number - 1;
+//				updateNumber
+//			}
+//		} 
+//		TextCountElement.textContent = number;		
+//	}
+
+	
+
+//	ButtonIncrease.addEventListener("click", () => {
+//		calcNumber(event);
+//	});
+	
+//	ButtonDecrease.addEventListener("click", () => {
+//		calcNumber(event);
+//	});
+
+
 }); // Afslutter: DOMContentLoaded
