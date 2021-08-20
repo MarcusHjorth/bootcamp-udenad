229196f01bda1839a0ba892e8725794e55ac7c0c
MarcusHjorth <U773559@rts-365.dk>
2021-08-20 09:25:35 +0200

    Add assignment 03b

--- eks_04a_objekter/js/index.js
+++ eks_04a_objekter/js/index.js
@@ -10,10 +10,23 @@ let ulElement = document.querySelector("#liste");
 function getName (array){
 	array.forEach((navn) => {
 		ulElement.innerHTML += `<li> ${navn} </li>`
-	});
-	
-}
+	});	
+};
 
 getName(names)
+
+
+
+
+//function getNaame (array){
+//	array.forEach((names) => {
+//		let li = document.createElement('li');
+//	   ulElement.appendChild(li);
+//		li.innerHTML += names;
+//	});
+//}
+
+//getNaame(names)
+
 
 }); // Afslutter: DOMContentLoaded
